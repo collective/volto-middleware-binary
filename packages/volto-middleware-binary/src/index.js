02b002c3d1f524848cd8fce45176480c04376ece
Mauro Amico <mauro.amico@gmail.com>
2025-09-07 17:59:07 +0200

    lint

--- packages/volto-middleware-binary/src/index.js
+++ packages/volto-middleware-binary/src/index.js
@@ -1,13 +1,16 @@
 const applyConfig = (config) => {
   // replace defaults files/image middleware
   if (__SERVER__) {
-    const proxyMiddleware = require('./expresss-middleware/proxy-binary').default;
+    const proxyMiddleware =
+      require('./expresss-middleware/proxy-binary').default;
     const middleware = proxyMiddleware();
     middleware.id = 'proxy-middleware';
     config.settings.expressMiddleware = [
       ...config.settings.expressMiddleware.filter(
         (m) =>
-          !['filesResourcesProcessor', 'imageResourcesProcessor'].includes(m.id),
+          !['filesResourcesProcessor', 'imageResourcesProcessor'].includes(
+            m.id,
+          ),
       ),
       middleware,
     ];
